b63c4cdd27d3fd10fbfa2b45947626c2a35595af
breadsmyth <amy.starrbright@gmail.com>
2025-09-07 20:35:44 -0600

    add commit hash to title

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,12 @@
-FPS = 24
+import subprocess
+
+
+current_hash = subprocess.run(
+    ['git', 'rev-parse', '--short', 'HEAD'],
+    capture_output=True,
+    text=True).stdout[:-1]
+
+
+FPS = 30
 RESOLUTION = (800, 800)
-TITLE = 'Project Juptier'
+TITLE = f'Project Juptier ({current_hash})'
