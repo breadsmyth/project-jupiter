d0c95594ad764f8185ebaaa1ac2beed35ab65cf7
breadsmyth <amy.starrbright@gmail.com>
2025-12-27 19:42:11 -0700

    Start making inventory panel

--- src/context/gameplay.py
+++ src/context/gameplay.py
@@ -1,10 +1,20 @@
-import draw
+import pygame
+
+import constants
+import draw.sprite
 
 
 def init():
-    global PLAYER_IMG
-    PLAYER_IMG = draw.sprite.load('peep.png')
+    # Create inventory panel
+    global inv_panel
+    inv_panel = pygame.Surface((
+        constants.RESOLUTION[0],
+        constants.RESOLUTION[1] // 3
+    ))
+
+    inv_panel.fill(constants.Color.FG)
 
 
 def do(screen):
-    draw.sprite.draw(screen, PLAYER_IMG, (100, 100))
+    # Draw inventory panel
+    screen.blit(inv_panel, (0, constants.RESOLUTION[1] // 3 * 2))
