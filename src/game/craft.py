00aea777559ce755630cc9461f2c2b2e12865cdf
breadsmyth <amy.starrbright@gmail.com>
2026-03-22 14:50:22 -0600

    add alias system, some items behave like others

--- src/game/craft.py
+++ src/game/craft.py
@@ -4,11 +4,19 @@ import os
 import game.progress
 
 
+with open(os.path.join('assets', 'data', 'alias.json')) as file:
+    alias_dict = json.loads(file.read())
+
 with open(os.path.join('assets', 'data', 'crafting.json')) as file:
     crafting_dict = json.loads(file.read())
 
 
 def check(item_id_a, item_id_b):
+    if item_id_a in alias_dict.keys():
+        item_id_a = alias_dict[item_id_a]
+    if item_id_b in alias_dict.keys():
+        item_id_b = alias_dict[item_id_b]
+    
     if item_id_b < item_id_a:
         item_id_a, item_id_b = item_id_b, item_id_a
     
